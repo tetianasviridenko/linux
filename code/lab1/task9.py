aaec9a4185fda37e401b7c42be9e5bcdc1563ee5
tetianasviridenko <tetiana.svyrydenko@student.karazin.ua>
2025-03-23 03:10:22 +0200

    added task9

---
+++ code/lab1/task9.py
@@ -0,0 +1,16 @@
+import platform
+import psutil, datetime
+def get_PC_info():
+    info = {
+            "System": platform.system(),
+            "Node Name": platform.node(),
+            "Release": platform.release(),
+            "Version": platform.version(),
+            "Machine": platform.machine(),
+            "Memory": psutil.virtual_memory()
+           }
+    return info
+if __name__ == "__main__":
+    PC_info = get_PC_info()
+    for key, value in PC_info.items():
+        print(f"{key}:{value}")
